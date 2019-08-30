655ae2f6302867f63675457f21dd722b48964668
Administrator <rpavankumar17@gmail.com>
2019-08-30 16:25:00 +0530

    Updates

---
+++ precache-manifest.2ea17773b991c589698a9a5c5691aa4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a816f837261ff26e336cc0556bd41d",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "c997e5776bbd1c1b1ddd",
+    "url": "/myapp/static/css/main.9de7ba13.chunk.css"
+  },
+  {
+    "revision": "ec1df89dbd402a0c1265",
+    "url": "/myapp/static/js/2.de6ad18f.chunk.js"
+  },
+  {
+    "revision": "c997e5776bbd1c1b1ddd",
+    "url": "/myapp/static/js/main.54ab5242.chunk.js"
+  },
+  {
+    "revision": "748c4a1a4543a6cd5083",
+    "url": "/myapp/static/js/runtime~main.93c24932.js"
+  }
+]);
