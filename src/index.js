7cf8e197357e8ba09754137dbab2b63c3bf8f8e2
Administrator <rpavankumar17@gmail.com>
2019-08-30 16:06:04 +0530

    my first git commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css'
+import ReactDOM from 'react-dom';
+//import * as serviceWorker from './serviceWorker';
+
+import MainPage from './pages/main';
+ReactDOM.render(<MainPage/>,
+ document.getElementById('root'));
+
+//serviceWorker.unregister();
